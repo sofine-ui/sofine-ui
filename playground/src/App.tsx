00cf56814db8114afac0aefe7b3b646f32f41cfb
xbmlz <997909544@qq.com>
2022-12-08 10:07:19 +0800

    feat: add playground

---
+++ playground/src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div class="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ playground/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import Solid from 'vite-plugin-solid'
+
+export default defineConfig({
+  plugins: [
+    Solid()
+  ]
+})
+
